8890388a3baab8b15da355db1e37b20a404342a3
p2006393 <arthur.desbiaux@etu.univ-lyon1.fr>
2024-02-14 22:24:57 +0100

    Modify the hero images

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,6 +13,10 @@ export const Hero = () => {
             </p>
             <a className={styles.contactBtn} href="mailto:arthurdbx93@gmail.com">Contactez-moi</a>
         </div>
-        <img className={styles.heroImg} src={ getImageURL("hero/pufferFish.jpg")} alt="Une photo de moi" />
+        {/* <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
+            <path fill="#148AD3" d="M41.8,-59.6C55,-48,67,-36.8,75.6,-21.8C84.3,-6.8,89.5,12,84.6,27.6C79.7,43.2,64.5,55.5,48.7,64C32.9,72.5,16.4,77.1,0.2,76.8C-16.1,76.6,-32.1,71.4,-47.4,62.8C-62.6,54.1,-77,41.9,-79.9,27.3C-82.8,12.7,-74.1,-4.3,-68.6,-22.7C-63.2,-41.1,-60.9,-61,-50,-73.2C-39.1,-85.5,-19.6,-90.2,-2.6,-86.6C14.3,-83,28.7,-71.1,41.8,-59.6Z" transform="translate(100 100)"></path>
+            <img className={styles.heroImg} src={ getImageURL("hero/Hero.png")} alt="Une photo de moi" />
+        </svg> */}
+        <img className={styles.heroImg} src={ getImageURL("hero/Hero.png")} alt="Une photo de moi" />
     </section>
 };
