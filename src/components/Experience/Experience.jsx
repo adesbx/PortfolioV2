7b24fb55c1fec50a2de0f179804af97215b77b6f
p2006393 <arthur.desbiaux@etu.univ-lyon1.fr>
2024-02-17 15:37:57 +0100

    Add experience

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Experience.module.css";
+import { getImageURL } from "../../utils";
+
+
+export const Experience = () => {
+    return (
+        <section className={styles.container} id="experience">
+            <h2 className={styles.title} >Experience</h2>
+            <div className={styles.content}>
+                <h3 className={styles.contentTitle} >I use a lot</h3>
+                <div className={styles.contentRow}>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/linux/linux-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"/>
+                </div>
+            </div>
+            <div className={styles.content}>
+                <h3 className={styles.contentTitle} >I use</h3>
+                <div className={styles.contentRow}>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg"/>
+                </div>
+            </div>
+            <div className={styles.content}>
+                <h3 className={styles.contentTitle} >I already use</h3>
+                <div className={styles.contentRow}>   
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angularjs/angularjs-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/django/django-plain.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/php/php-original.svg"/>
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/prolog/prolog-original.svg"/>
+                </div>
+            </div>
+        </section>
+    )   
+};
