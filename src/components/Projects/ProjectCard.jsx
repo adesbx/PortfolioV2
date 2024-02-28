e0649716337ee3eaf11bd303ebed7353d70c9f4c
p2006393 <arthur.desbiaux@etu.univ-lyon1.fr>
2024-02-28 17:39:24 +0100

    Projects parts

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./ProjectCard.module.css";
+import { getImageURL } from "../../utils";
+
+export const ProjectCard = ({project : {title, image, description, skills, source}}) => {
+
+    return(
+        <div className={styles.container}>
+            <img  src={getImageURL(image)} alt={`Image of ${title}`} className={styles.image}/>
+            <h3 className={styles.title}>{title}</h3>
+            <p className={styles.description}>{description}</p>
+            <ul className={styles.skills}>
+                {skills.map((skill, id) => {
+                    return (
+                    <li key={id} className={styles.skill} >
+                        {skill}
+                    </li>
+                    );
+                })}
+            </ul>
+            <div className={styles.links}>
+                <a href={source} className={styles.link}>Source</a>
+            </div>
+        </div>
+    )
+};
+
